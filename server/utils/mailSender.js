012215907cd5718012a9577a0eedd1ec61bdd910
prashantchauhan1602 <prashantrana1602@gmail.com>
2025-06-16 01:35:29 +0530

    Initial commit to push the code

---
+++ server/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+const mailSender = async (email, title, body) => {
+  try {
+    console.log("Inside transporter !!")
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      secure: false,
+    })
+    console.log(" sendmail start  !!")
+    let info = await transporter.sendMail({
+      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${title}`, // Subject line
+      html: `${body}`, // html body
+    })
+    // console.log(info.response)
+    return info
+  } catch (error) {
+    console.log(error.message)
+    return error.message
+  }
+}
+
+module.exports = mailSender
